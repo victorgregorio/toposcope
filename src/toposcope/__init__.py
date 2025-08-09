99f34a6cae7407d79c8a5d2b03d5728f5172a626
Victor Gregorio <victor@victorgregorio.com>
2025-08-09 18:51:19 -0400

    use setuptools-scm to derive version from git

--- src/toposcope/__init__.py
+++ src/toposcope/__init__.py
@@ -1,5 +1,8 @@
-__all__ = [
-    "__version__",
-]
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
 
-__version__ = "0.6.0"
+__all__ = ["__version__"]
+
+try:
+    __version__ = _pkg_version("toposcope")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
